0d59819697847781c244e0e2181a839710c4ee29
Dustin Sallings <dustin@spy.net>
2008-12-08 10:18:57 -0800

    Implemented verify_credentials.

--- lib/twitter.py
+++ lib/twitter.py
@@ -15,7 +15,15 @@ from twisted.web import client
 
 import txml
 
+BASE_URL="http://twitter.com"
 SEARCH_URL="http://search.twitter.com/search.atom"
+
+def makeAuthHeader(username, password, headers=None):
+    if not headers:
+        headers = {}
+    authorization = base64.encodestring('%s:%s' % (username, password))[:-1]
+    headers['Authorization'] = "Basic %s" % authorization
+    return headers
 
 def __urlencode(h):
     rv = []
@@ -23,6 +31,16 @@ def __urlencode(h):
         rv.append('%s=%s' %
             (urllib.quote(k.encode("utf-8")), urllib.quote(v.encode("utf-8"))))
     return '&'.join(rv)
+
+def __post(user, password, path, args={}):
+    h = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return client.getPage((BASE_URL + "%s") % path, method='POST',
+        postdata=__urlencode(args),
+        headers=makeAuthHeader(user, password, h))
+
+def verify_credentials(username, password):
+    "Verify a user's credentials."
+    return __post(username, password, "/account/verify_credentials.xml")
 
 def search(query, delegate):
     "Perform a search query.  Get results in a deferred."
