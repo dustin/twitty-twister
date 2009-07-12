48c8fce8051902b866df767007a15b838081693b
Kévin Dunglas <dunglas@macbook-de-kevin-dunglas.local>
2009-07-12 23:19:25 +0200

    New test file for the show_user method with OAuth.

---
+++ example/show-user-oauth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2009  Kevin Dunglas <dunglas@gmail.com>
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], '..', 'lib'))
+sys.path.append('lib')
+
+from twisted.internet import reactor
+
+import twitter
+import oauth
+
+def gotUser(u):
+    print "Got a user: %s" % u
+
+consumer = oauth.OAuthConsumer(sys.argv[1], sys.argv[2])
+token = oauth.OAuthToken(sys.argv[3], sys.argv[4])
+
+twitter.Twitter(consumer=consumer, token=token).show_user(sys.argv[5]).addCallback(
+    gotUser).addBoth(lambda x: reactor.stop())
+
+reactor.run()
