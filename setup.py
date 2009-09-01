b106d4fdaf1667061879dd170ddeec1bde2042aa
Ralph Meijer <ralphm@ik.nu>
2009-09-01 10:43:19 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='twittytwister',
+    version='0.1',
+    description='Twitter client for Twisted Python',
+    author='Dustin Sallings',
+    author_email='dustin@spy.net',
+    url='http://github.com/dustin/twitty-twister/',
+    license='MIT',
+    platforms='any',
+    packages=['twittytwister'],
+)
