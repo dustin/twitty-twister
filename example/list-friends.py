3b24b0e295a12e7b9bdccec830e63b041575d157
Eduardo Habkost <ehabkost@raisama.net>
2009-12-16 01:51:11 -0200

    examples/list-friends.py: show errors
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- example/list-friends.py
+++ example/list-friends.py
@@ -17,11 +17,15 @@ import twitter
 def gotUser(user):
     print "User:  %s (%s)" % (user.name, user.screen_name)
 
+def error(e):
+    print "ERROR: %s" % (e)
+    reactor.stop()
+
 un=None
 if len(sys.argv) > 3:
     un=sys.argv[3]
 
-twitter.Twitter(sys.argv[1], sys.argv[2]).list_friends(gotUser, un).addBoth(
-    lambda x: reactor.stop())
+twitter.Twitter(sys.argv[1], sys.argv[2]).list_friends(gotUser, un).addCallbacks(
+    lambda x: reactor.stop(), error)
 
 reactor.run()
