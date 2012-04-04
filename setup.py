e9555786dff4b3afcd4cf17a7b3959ba4bea5085
shaunsephton <shaunsephton@gmail.com>
2012-04-04 11:56:32 +0200

    use setuptools, version bump to 0.1.1, install dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='twittytwister',
-    version='0.1',
+    version='0.1.1',
     description='Twitter client for Twisted Python',
     author='Dustin Sallings',
     author_email='dustin@spy.net',
@@ -9,4 +9,9 @@ setup(name='twittytwister',
     license='MIT',
     platforms='any',
     packages=['twittytwister'],
+    install_requires=[
+        'oauth',
+        'simplejson',
+        'Twisted',
+    ]
 )
